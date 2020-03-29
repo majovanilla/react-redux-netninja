755d6a23566e9b63f27c97f5cf9b257e7048496e
majovainilla <maria_reyes@me.com>
2020-03-29 12:08:03 -0600

    Add switch to navbar and styled posts

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
+import Pokeball from '../pokeball.png'
 
 class Home extends React.Component {
   state = {
@@ -21,9 +22,10 @@ class Home extends React.Component {
       posts.map(post => {
         return (
           <div className="post card" key={post.id}>
+            <img src={Pokeball} alt="pokeball" />
             <div className="card-content">
               <Link to={'/' + post.id} >
-                <span className="title">{post.title}</span>
+                <span className="title red-text">{post.title}</span>
               </Link>
               <p>{post.body}</p>
             </div>
