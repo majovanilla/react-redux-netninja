b8e4732f7936a6f95c7522e30d74cc7cc0275808
majovainilla <maria_reyes@me.com>
2020-03-29 11:38:49 -0600

    Add hook to home component to render posts once mounted

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,17 +2,39 @@ import React from 'react';
 import axios from 'axios';
 
 class Home extends React.Component {
+  state = {
+    posts: []
+  }
+
   componentDidMount() {
     axios.get('https://jsonplaceholder.typicode.com/posts')
       .then(response => {
-        console.log(response)
+        this.setState({
+          posts: response.data.slice(0, 10)
+        })
       })
   }
   render() {
+    const { posts } = this.state;
+    const postList = posts.length ? (
+      posts.map(post => {
+        return (
+          <div className="post card" key={post.id}>
+            <div className="card-content">
+              <span className="title">{post.title}</span>
+              <p>{post.body}</p>
+            </div>
+          </div>
+        )
+      })
+    ) : (
+        <div className="center">Not posts yet</div>
+      )
+
     return (
       <div className="container">
         <h4 className="center">Home</h4>
-        <p>lorem lorem Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus vel consequatur nostrum quam aperiam labore praesentium officiis iusto! Hic veritatis nobis at molestias obcaecati ullam asperiores nihil sunt fugit quis. </p>
+        {postList}
       </div>
     )
   }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import { Link, NavLink, withRouter } from 'react-router-dom';
 
-const Navbar = (props) => {
-  setTimeout(() => {
-    props.history.push('/about')
-  }, 2000);
-
+const Navbar = () => {
   return (
     <nav className="nav-wrapper red darken-3">
       <div className="container">
-        <a className="brand-logo">Multipage App</a>
+        <a className="brand-logo" href="#">Multipage App</a>
         <ul className="right">
-          <li><Link  to="/">Home</Link></li>
+          <li><Link to="/">Home</Link></li>
           <li><NavLink to="/about">About</NavLink></li>
           <li><NavLink to="/contact">Contact</NavLink></li>
         </ul>
