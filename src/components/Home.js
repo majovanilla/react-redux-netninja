29930f1493c99f66ac5e5c9e1f2ce0c5486eabc1
majovainilla <maria_reyes@me.com>
2020-03-29 11:31:19 -0600

    Add axios package and basic function to home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import axios from 'axios';
 
-const Home = () => {
-  return (
-    <div className="container">
-      <h4 className="center">Home</h4>
-      <p>lorem lorem Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus vel consequatur nostrum quam aperiam labore praesentium officiis iusto! Hic veritatis nobis at molestias obcaecati ullam asperiores nihil sunt fugit quis. </p>
-    </div>
-  )
+class Home extends React.Component {
+  componentDidMount() {
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+      .then(response => {
+        console.log(response)
+      })
+  }
+  render() {
+    return (
+      <div className="container">
+        <h4 className="center">Home</h4>
+        <p>lorem lorem Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus vel consequatur nostrum quam aperiam labore praesentium officiis iusto! Hic veritatis nobis at molestias obcaecati ullam asperiores nihil sunt fugit quis. </p>
+      </div>
+    )
+  }
 }
 
 export default Home;
